7118af7d939fb5de124a5b0bd40114840c952667
dklinenberg2020 <dklinenberg@ucsb.edu>
2020-09-07 12:44:10 -0700

    update pse

--- 2020_f_psexample_v1/test.R
+++ 2020_f_psexample_v1/test.R
@@ -4,7 +4,11 @@ PERMID         <- 1212
 
 fakedata       <- read.csv("fakedata.csv")
 
+# Problem 1
+
 invoice.kansas <- 11 #sum(fakedata$State=="Kansas")
+
+# Problem 2
 
 P1             <- sum(fakedata$Paid == "Yes")
 
